8ac662f513c837b508d17020ba79aa0451277b09
Ксения Ковалева <kovalevaksenia21@gmail.com>
2024-11-30 17:23:29 +0300

    добавление widget.py

---
+++ src/widget.py
@@ -0,0 +1,19 @@
+import masks
+
+def mask_account_card(user_input):
+    split_input = user_input.split()
+    if len(split_input) == 2:
+      if split_input[0].lower() == "счет":
+       new_input = "".join(split_input[-1])
+       return print(masks.get_mask_account(new_input))
+      else:
+         new_input = "".join(split_input[-1])
+         return print(masks.get_masck_card_number(new_input))
+    else:
+      print("Вы ввели неккоректные данные")
+
+
+def get_date(user_date):
+    new_date = user_date.split("T")
+    date = new_date[0].split("-")
+    return print(f"{date[-1]}.{date[-2]}.{date[-3]}")
