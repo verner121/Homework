14d8b4d9b26ce2e8d752a38c41eb2fe877a14b14
Ксения Ковалева <kovalevaksenia21@gmail.com>
2024-11-30 17:52:02 +0300

    конечный результат функции

--- src/masks.py
+++ src/masks.py
@@ -4,25 +4,25 @@ CONST_CARD_NUMB = 16
 CONST_ACCOUNT_NUMB = 20
 
 
-def get_masck_card_number(card_number: Union[int, str]) -> None | str:
+def get_masck_card_number(new_input: Union[int, str]) -> None | str:
     """Функция, принимаюшая номер карты, на выходе - маска карты"""
-    str_number = str(card_number)
+    str_number = str(new_input)
     if str_number.isdigit():
         if len(str_number) == CONST_CARD_NUMB:
             return f"{str_number[:4]} {str_number[4:6]} ** **** {str_number[12:]}"
         else:
-            print("Вы ввели недостаточно цифр")
+            return print("Вы ввели недостаточно цифр")
     else:
-         print("Вы ввели не число")
+         return print("Вы ввели не число")
 
 
-def get_mask_account(card_account: Union[int, str]) -> None | str:
+def get_mask_account(new_input: Union[int, str]) -> None | str:
     """Функция принимающая счет карты и на выходе - маску счета"""
-    str_account = str(card_account)
+    str_account = str(new_input)
     if str_account.isdigit():
         if len(str_account) == CONST_ACCOUNT_NUMB:
             return f"**{str_account[-4:]}"
         else:
-            print("Вы ввели недостаточно цифр")
+           return  print("Вы ввели недостаточно цифр")
     else:
-        print("Введите номер лицевого счета")
+       return  print("Введите номер лицевого счета")
--- src/widget.py
+++ src/widget.py
@@ -1,19 +1,24 @@
-import masks
+from typing import Union
 
-def mask_account_card(user_input):
+from src import masks
+
+
+def mask_account_card(user_input: Union[str]) -> None | str:
+    """Функция, принимающая номер или счет карты с названием банка"""
     split_input = user_input.split()
     if len(split_input) == 2:
-      if split_input[0].lower() == "счет":
-       new_input = "".join(split_input[-1])
-       return print(masks.get_mask_account(new_input))
-      else:
-         new_input = "".join(split_input[-1])
-         return print(masks.get_masck_card_number(new_input))
+        if split_input[0].lower() == "счет":
+            new_input = "".join(split_input[-1])
+            return masks.get_mask_account(new_input)
+        else:
+            new_input = "".join(split_input[-1])
+            return masks.get_masck_card_number(new_input)
     else:
-      print("Вы ввели неккоректные данные")
+        return print("Вы ввели неккоректные данные")
 
 
-def get_date(user_date):
+def get_date(user_date: Union[str]) -> None | str:
+    """Функция, принимающая дату"""
     new_date = user_date.split("T")
     date = new_date[0].split("-")
     return print(f"{date[-1]}.{date[-2]}.{date[-3]}")
