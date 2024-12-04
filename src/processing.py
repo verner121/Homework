4c7682b78f6f4ce3c3fde6d91f2cef711fccaf8f
Ксения Ковалева <kovalevaksenia21@gmail.com>
2024-12-05 00:56:35 +0300

    исправление ошибок_2

--- src/processing.py
+++ src/processing.py
@@ -1,16 +1,16 @@
 from typing import Any
 
 
-def filter_by_state(user_dictionary: list[dict[str, Any]], state: str = "EXECUTED") -> Any:
+def filter_by_state(user_dictionary: list[dict[str, Any]], state: str = "EXECUTED") -> list[dict[str, Any]]:
     """Функция для выведения данных по определенному значению"""
     new_dictionary = []
-    for i in user_dictionary:
-        if i["state"] == state:
-            new_dictionary.append(i)
-    return print(new_dictionary)
+    for dictionary in user_dictionary:
+        if dictionary["state"] == state:
+            new_dictionary.append(dictionary)
+    return new_dictionary
 
 
-def sort_by_date(user_dictionary: list[dict[str, Any]], reverse: bool = True) -> Any:
-    """Функция, сортирующая список словаlist[dict[str, Any]]рей по дате"""
-    new_date = sorted(user_dictionary, key=lambda i: i["date"], reverse=reverse)
-    return print(new_date)
+def sort_by_date(user_dictionary: list[dict[str, Any]], reverse: bool = True) -> list[dict[str, Any]]:
+    """Функция, сортирующая список по дате"""
+    new_date = sorted(user_dictionary, key=lambda dictionary: dictionary["date"], reverse=reverse)
+    return new_date
