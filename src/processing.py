af3a1c3b78d1957c5b766a5dc4f66d1f3be803da
Ксения Ковалева <kovalevaksenia21@gmail.com>
2024-12-04 18:30:26 +0300

    add readme.md

--- src/processing.py
+++ src/processing.py
@@ -1,10 +1,16 @@
-def filter_by_state(user_dictionary, state = "EXECUTED"):
-  new_dictionary = []
-  for i in user_dictionary:
-    if i["state"] == state:
-      new_dictionary.append(i)
-  return print(new_dictionary)
+from typing import Any
 
-def sort_by_date(user_dictionary, reverse = True):
-  new_date = sorted(user_dictionary, key = lambda i: i["date"], reverse = reverse)
-  return print(new_date)
+
+def filter_by_state(user_dictionary: list[dict[str, Any]], state: str = "EXECUTED") -> Any:
+    """Функция для выведения данных по определенному значению"""
+    new_dictionary = []
+    for i in user_dictionary:
+        if i["state"] == state:
+            new_dictionary.append(i)
+    return print(new_dictionary)
+
+
+def sort_by_date(user_dictionary: list[dict[str, Any]], reverse: bool = True) -> Any:
+    """Функция, сортирующая список словаlist[dict[str, Any]]рей по дате"""
+    new_date = sorted(user_dictionary, key=lambda i: i["date"], reverse=reverse)
+    return print(new_date)
