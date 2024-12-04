d673d6774f9f8ac0339b1157539c2487cde404fe
Ксения Ковалева <kovalevaksenia21@gmail.com>
2024-12-04 17:28:26 +0300

    добавление функции sort_by_date

--- src/processing.py
+++ src/processing.py
@@ -5,3 +5,6 @@ def filter_by_state(user_dictionary, state = "EXECUTED"):
       new_dictionary.append(i)
   return print(new_dictionary)
 
+def sort_by_date(user_dictionary, reverse = True):
+  new_date = sorted(user_dictionary, key = lambda i: i["date"], reverse = reverse)
+  return print(new_date)
