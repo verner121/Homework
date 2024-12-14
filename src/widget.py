b52216c49e333632e1de951effe5bca84c8feb8d
Ксения Ковалева <kovalevaksenia21@gmail.com>
2024-12-14 19:08:28 +0300

    исправление ошибок

--- src/widget.py
+++ src/widget.py
@@ -3,18 +3,24 @@ from typing import Union
 from src import masks
 
 
-def mask_account_card(user_input: Union[str]) -> None | str:
-    """Функция, принимающая номер или счет карты с названием банка"""
-    split_input = user_input.split()
-    if len(split_input) == 2:
-        if split_input[0].lower() == "счет":
-            new_input = "".join(split_input[-1])
-            return masks.get_mask_account(new_input)
-        else:
-            new_input = "".join(split_input[-1])
-            return masks.get_masck_card_number(new_input)
+def mask_account_card(user_input: Union[str]) -> str:
+    """Функция.принимающая номер карты или счета, на выходе - маска"""
+    if user_input != "":
+      split_input = user_input.split()
+      if len(split_input) == 2:
+          if len(split_input[1]) == 20 or 16:
+            if split_input[0].lower() == "счет":
+                new_input = "".join(split_input[-1])
+                return masks.get_mask_account(new_input)
+            else:
+                new_input = "".join(split_input[-1])
+                return masks.get_masck_card_number(new_input)
+          else:
+              return "Вы ввели недостаточно цифр"
+      else:
+          return "Вы ввели неккоректные данные"
     else:
-        return print("Вы ввели неккоректные данные")
+      return "Введите счет или номер карты"
 
 
 def get_date(user_date: Union[str]) -> str:
