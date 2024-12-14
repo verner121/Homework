4341ee1c70beb948e18865f172c9d67a2ea93b62
Ксения Ковалева <kovalevaksenia21@gmail.com>
2024-12-14 19:57:51 +0300

    исправление функций

--- src/masks.py
+++ src/masks.py
@@ -4,25 +4,25 @@ CONST_CARD_NUMB = 16
 CONST_ACCOUNT_NUMB = 20
 
 
-def get_masck_card_number(new_input: Union[int, str]) -> None | str:
+def get_masck_card_number(new_input: Union[int, str]) ->  str:
     """Функция, принимаюшая номер карты, на выходе - маска карты"""
     str_number = str(new_input)
     if str_number.isdigit():
         if len(str_number) == CONST_CARD_NUMB:
             return f"{str_number[:4]} {str_number[4:6]} ** **** {str_number[12:]}"
         else:
-            return print("Вы ввели недостаточно цифр")
+            return "Вы ввели недостаточно цифр"
     else:
-         return print("Вы ввели не число")
+         return "Вы ввели не число"
 
 
-def get_mask_account(new_input: Union[int, str]) -> None | str:
+def get_mask_account(new_input: Union[int, str]) ->  str:
     """Функция принимающая счет карты и на выходе - маску счета"""
     str_account = str(new_input)
     if str_account.isdigit():
         if len(str_account) == CONST_ACCOUNT_NUMB:
             return f"**{str_account[-4:]}"
         else:
-           return  print("Вы ввели недостаточно цифр")
+           return  "Вы ввели недостаточно цифр"
     else:
-       return  print("Введите номер лицевого счета")
+       return  "Введите номер лицевого счета"
